04e2d533ee3b4fd9932d3de1063931b70d92306f
FranFT <franfajardotoril@gmail.com>
2026-02-11 21:37:20 +0100

    Adding Invoice Line Items  model

--- src/invoice_model.py
+++ src/invoice_model.py
@@ -1,10 +1,25 @@
 from datetime import datetime
-from pydantic import BaseModel
+from pydantic import BaseModel, Field, PositiveInt, List
+
+class LineItem(BaseModel):
+    """
+    Defining individual line items.
+    """
+    description: str = Field(..., description="Description of the product or service.")
+    quantity: PositiveInt = Field(..., description="Number of items.")
+    unit_price: float = Field(..., description="Price per single unit")
+    total: float = Field(..., description="Total amount for this line item.")
 
 class Invoice(BaseModel):
-    """Invoice information"""
+    """
+    Defining main invoice model
+    """
     vendor_name: str
     invoice_number: int
     invoice_date: datetime.date
-    due_date: datetime.date
-    total_amount: float
+    line_items: List[LineItem] = Field(..., description="List of line items in the invoice.")
+
+    # Calculate total amount automatically.
+    @property
+    def total_amount(self) -> float:
+        return sum(item.total for item in self.line_items)
